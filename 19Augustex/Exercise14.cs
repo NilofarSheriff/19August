d466832f3f448d7de34aafb804e506db6cf728c4
Nilofar <nilofarsheriff@gmail.com>
2022-08-19 22:41:57 +0530

    Coorections mad in 14 ex.

--- 19Augustex/Exercise14.cs
+++ 19Augustex/Exercise14.cs
@@ -12,49 +12,51 @@ namespace _19Augustex
         {
             Console.WriteLine("Enter an integer");
             int a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Menu \n 1.Check Zero or greater than 100 \n 2.Check odd or even \n 3.Check Multiple of 10 or not");
+            Console.WriteLine("Menu \n 1.Check the input \n 2.Exit");
 
             int userchoice=Convert.ToInt32(Console.ReadLine());
             switch (userchoice)
             {
                 case 1:
                     {
+
                         if (a == 0)
                         {
                             Console.WriteLine("Input is Zero");
+
                         }
                         else if (a > 100)
                         {
-                            Console.WriteLine("Too large Input");
+                            Console.WriteLine("Too large input");
+                        }
+                        else if (a % 2 != 0)
+                        {
+                            Console.WriteLine("Input {0} is odd Number", a);
+
+                        }
+                        else if (a % 2 == 0)
+
+                        {
+                            if (a % 10 == 0)
+                            {
+                                Console.WriteLine("Input {0} is a Multiple of 10", a);
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("Input {0} is Even Number", a);
+                            }
+                            
                         }
                         break;
                     }
                 case 2:
                     {
-                        if (a%2 != 0)
-                        {
-                            Console.WriteLine("Input {0} is a odd number",a);
-                        }
-                        else if (a%2== 0)
-                        {
-                            Console.WriteLine("Input {0} is a even number",a);
-                        }
-                        break;
-                    }
-                case 3:
-                    {
-                        if (a % 10 != 0)
-                        {
-                            Console.WriteLine("Input {0} is not a multiple of 10", a);
-                        }
-                        else if (a % 10 == 0)
-                        {
-                            Console.WriteLine("Input {0} is a multiple of 10", a);
-                        }
+                        Environment.Exit(0);
                         break;
 
                     }
-                
+                 
                        
                   
             }
